d90a59d7dcabcee1e96055b013db87eaaecab793
Fmlauze <77634656+Fmlauze@users.noreply.github.com>
2022-10-25 15:02:36 +0200

    1 file modified

--- Components/navbar/Navbar.tsx
+++ Components/navbar/Navbar.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
 import './Navbar.css';
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 const Navbar: React.FunctionComponent = () => {
   return <div>test</div>;
